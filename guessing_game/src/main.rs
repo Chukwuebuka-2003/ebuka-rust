e262b5dbe40d1ca82347dd0dff558d90000498e1
Chukwuebuka-2003 <ebulamicheal@gmail.com>
2024-04-04 19:19:18 +0000

    How to build a guess the number game in Rust

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,36 @@
+use std::cmp::Ordering;
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop{
+        println!("Please input your guess");
+        
+        let mut guess = String::new();
+        
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => print!("Too large"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+    }
+}
