99dfc95f33f2c254fe65d8eb78c93f4cc7d82b6c
Chukwuebuka-2003 <ebulamicheal@gmail.com>
2024-04-08 23:50:46 +0000

    An update

---
+++ temp_convert/target/debug/deps/temp_convert-14c6034a480ca4b1.d
@@ -0,0 +1,5 @@
+/home/stocks/ebuka_rust/temp_convert/target/debug/deps/temp_convert-14c6034a480ca4b1: src/main.rs
+
+/home/stocks/ebuka_rust/temp_convert/target/debug/deps/temp_convert-14c6034a480ca4b1.d: src/main.rs
+
+src/main.rs:
--- temp_convert/target/debug/temp_convert.d
+++ temp_convert/target/debug/temp_convert.d
@@ -1,1 +1,1 @@
-/home/ebuka/ebuka-rust/temp_convert/target/debug/temp_convert: /home/ebuka/ebuka-rust/temp_convert/src/main.rs
+/home/stocks/ebuka_rust/temp_convert/target/debug/temp_convert: /home/stocks/ebuka_rust/temp_convert/src/main.rs
