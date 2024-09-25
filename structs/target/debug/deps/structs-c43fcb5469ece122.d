a56a61efc3657ad92c311c2f61062813fcc5a6c2
Chukwuebuka (Gaus Octavio ) <56232734+Chukwuebuka-2003@users.noreply.github.com>
2024-09-25 05:54:35 +0000

    Delete structs/target directory

--- structs/target/debug/deps/structs-c43fcb5469ece122.d
+++
@@ -1,5 +0,0 @@
-/home/ebuka/ebuka-rust/structs/target/debug/deps/libstructs-c43fcb5469ece122.rmeta: src/main.rs
-
-/home/ebuka/ebuka-rust/structs/target/debug/deps/structs-c43fcb5469ece122.d: src/main.rs
-
-src/main.rs:
--- structs/target/debug/structs.d
+++
@@ -1,1 +0,0 @@
-/home/ebuka/ebuka-rust/structs/target/debug/structs: /home/ebuka/ebuka-rust/structs/src/main.rs
